7ca97193a3d4842d9aaa6f0150a5a5ce2f272f06
Bhdrpkcn <bhdrpkcn@gmail.com>
2023-11-04 16:37:24 +0300

    first commit test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
 
 function App() {
+  const [tariffs, setTariffs] = useState([]);
+
+  useEffect(() => {
+    async function fetchTariffs() {
+      try {
+        const response = await fetch("https://demo6684249.mockable.io/tariffs");
+        if (!response.ok) {
+          throw new Error(
+            `Error fetching tariffs: ${response.status} ${response.statusText}`
+          );
+        }
+        const data = await response.json();
+        setTariffs(data.tariffs);
+      } catch (error) {
+        console.error("Error fetching tariffs:", error);
+      }
+    }
+
+    fetchTariffs();
+  }, []); // The empty array [] makes this effect run once after the initial render.
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {tariffs.map((tariff) => (
+          <div className="tariffs">
+            <hr />
+            <h4>
+              brand : {tariff.brand}(-id:{tariff.id})
+            </h4>
+            <h6>
+              <ul>tariff name : {tariff.name}</ul>
+              <ul>connection type: {tariff.type}</ul>
+              <ul>
+                speed: {tariff.download_speed}-{tariff.upload_speed}
+              </ul>
+              <ul>offers: {tariff.offers}</ul>
+              <h2>speed: {tariff.price}</h2>
+            </h6>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
